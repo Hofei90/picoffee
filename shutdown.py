2c1f00ca75825f2033a148f22b54bbeb3d10bb5b
Hofei90 <29521028+Hofei90@users.noreply.github.com>
2017-09-07 20:51:42 +0200

    meigrafd Version verwendet

--- shutdown.py
+++ shutdown.py
@@ -1,32 +1,47 @@
-# shutdown script for Raspberry Pi
-# watch LOW level on pin 5 to enter sleep mode
-# status led on pin 7: ON = ready, BLINK = confirm button
-
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# v0.3 by meigrafd
+#
 import RPi.GPIO as GPIO
-import os
-import time
+import time, signal
+from subprocess import call
+#------------------------------------------------------------------------
 
-# use the pin number as on the raspi board
-
+# use the raspi board pin number
 GPIO.setmode(GPIO.BOARD)
+# use the gpio number
+#GPIO.setmode(GPIO.BCM)
 
-# set pin 7 as output and HIGH, pin 5 is input
+shutdownPin = 5 # with GPIO.BOARD, pin#5 is gpio3
+ledPin = 7 # with GPIO.BOARD, pin#7 is gpio4
 
-GPIO.setup(7, GPIO.OUT)
-GPIO.output(7, True)
-GPIO.setup(5, GPIO.IN)
+# how often should the LED blink?
+blinkRepeat = 5
+# blink speed (sec)?
+blinkSpeed = 0.5
 
-# start the loop for every .5 seconds, waiting for LOW on pin 5
-# then 2 short flashes with led to confirm and shutdown to sleep mode
+#------------------------------------------------------------------------
 
-while True:
-    if not (GPIO.input(5)):
-        GPIO.output(7, False)
-        time.sleep(.1)
-        GPIO.output(7, True)
-        time.sleep(.1)
-        GPIO.output(7, False)
-        time.sleep(.1)
-        GPIO.output(7, True)
-        os.system("sudo shutdown -h now")
-    time.sleep(.5)
+GPIO.setup(ledPin, GPIO.OUT)
+GPIO.output(ledPin, True)
+GPIO.setup(shutdownPin, GPIO.IN)
+
+def blinkLED(pin, speed):
+    GPIO.output(pin, False)
+    time.sleep(speed)
+    GPIO.output(pin, True)
+
+def Interrupt_event(pin):
+    if GPIO.input(shutdownPin):
+        for x in range(0, (blinkRepeat + 1)):
+            blinkLED(int(ledPin), float(blinkSpeed))
+        #run command
+        call('poweroff', shell=False)
+
+try:
+    GPIO.add_event_detect(shutdownPin, GPIO.RISING, callback=Interrupt_event, bouncetime=150)
+    #keep script running
+    signal.pause()
+except (KeyboardInterrupt, SystemExit):
+    GPIO.cleanup() 
