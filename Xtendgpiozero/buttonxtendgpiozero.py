979e993ea7910c0ef0ccc0888157493e218fc9dc
Hofei90 <29521028+Hofei90@users.noreply.github.com>
2018-09-13 09:41:38 +0200

    xtend modul while entfernt - alternative geschrieben und getestet

--- Xtendgpiozero/buttonxtendgpiozero.py
+++ Xtendgpiozero/buttonxtendgpiozero.py
@@ -1,24 +1,30 @@
 import gpiozero
-import datetime
+import time
 
 
 class Button(gpiozero.Button):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._event = False
-        self.event_time = 0
-
-    def set_event(self):
-        start = datetime.datetime.now()
-        ende = datetime.datetime.now()
-        while self.is_pressed:
-           ende = datetime.datetime.now()
-        self.event_time = (ende - start).total_seconds()
-        # print(self.event_time)
-        self._event = True
+        self.event_time = None
+        self._pressed_at = None
 
     def check_status(self):
         status = self._event
         # print("status: {}".format(status))
         self._event = False
         return status
+
+    def when_pressed_(self):
+        self._pressed_at = time.time()
+        self._event = True
+
+    def when_released_(self):
+        self.event_time = time.time() - self._pressed_at
+
+    def get_event_time(self):
+        event_time = self.event_time
+        self.event_time = None
+        return event_time
+
+
